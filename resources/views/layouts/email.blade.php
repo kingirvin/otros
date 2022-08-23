45c2eaa4f9901bc613d4474c86fecdbea4c8ee92
Jose Cortijo <josecortijo2000@hotmail.com>
2022-08-23 13:44:56 -0500

    Fix email

--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -71,6 +71,12 @@
     .m-0 {
         margin: 0;
     }
+    .ms-auto{
+        margin-left: auto;
+    }
+    .me-auto{
+        margin-right: auto;
+    }
     .lh-sm {
         line-height: 1.25;
     }
@@ -98,10 +104,7 @@
     }
     .align-items-center {
         align-items: center;
-    }
-    .justify-content-evenly {
-        justify-content: space-evenly;
-    }
+    }    
     .d-flex {
         display: flex;
     }
@@ -146,7 +149,7 @@
                     @yield('contenido')
                 </div>
                 <div class="d-flex justify-content-evenly align-items-center">
-                    <a href="mailto: oti@unamad.edu.pe" class="d-block text-center" style="text-decoration: none;">
+                    <a href="mailto: oti@unamad.edu.pe" class="d-block text-center ms-auto me-auto" style="text-decoration: none;">
                         <div class="h3 m-0">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle" viewBox="0 0 16 16" style="height: 25px; width: 25px;">
                                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
@@ -155,7 +158,7 @@
                         </div>            
                         Ayuda            
                     </a>
-                    <a href="https://www.unamad.edu.pe/" target="_blank" class="d-block text-center" style="text-decoration: none;">
+                    <a href="https://www.unamad.edu.pe/" target="_blank" class="d-block text-center ms-auto me-auto" style="text-decoration: none;">
                         <div class="h3 m-0">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-globe" viewBox="0 0 16 16" style="height: 25px; width: 25px;">
                                 <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z"/>
@@ -163,7 +166,7 @@
                         </div> 
                         Web                       
                     </a>
-                    <a href="https://www.facebook.com/unamad.oficial/" target="_blank" class="d-block text-center" style="text-decoration: none;">
+                    <a href="https://www.facebook.com/unamad.oficial/" target="_blank" class="d-block text-center ms-auto me-auto" style="text-decoration: none;">
                         <div class="h3 m-0">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16" style="height: 25px; width: 25px;">
                                 <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
