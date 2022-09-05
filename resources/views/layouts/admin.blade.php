678999f95ff078ed349ba1225579eee6acf4ce8c
Jose Cortijo <josecortijo2000@hotmail.com>
2022-09-05 08:29:04 -0500

    icono 2

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">    
     <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}" />
+    <link rel="icon" sizes="192x192" href="{{ asset('img/unamad_firma.png') }}">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Titulo') }} - @yield('titulo')</title>
